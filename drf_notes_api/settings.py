2bb08a3fcd726a4f32996128e02a0ae13ef65177
Emily Y Leung <30677901+emilyyleung@users.noreply.github.com>
2022-04-09 11:04:32 +1000

    Move secret key and fix typos

--- drf_notes_api/settings.py
+++ drf_notes_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dapr6620llm_qtzp3(!+)sm*ka6)!i4z8vopl-c$@7zkm^b5lk'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
