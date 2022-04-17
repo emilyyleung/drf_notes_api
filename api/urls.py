8f96a5e9b61d4eb20f24ab81e286df1cdb3c269a
Emily Y Leung <30677901+emilyyleung@users.noreply.github.com>
2022-04-17 17:20:02 +1000

    Django REST Framework GET views

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
+    path('notes/', views.notesList, name="notes"),
+    path('notes/<str:pk>', views.noteDetail, name="note"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from . serializers import *
+
 from django.http import JsonResponse
 
+@api_view(['GET'])
 def getRoutes(request):
 
     routes = [
@@ -36,4 +41,16 @@ def getRoutes(request):
         },
     ]
 
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
+
+@api_view(['GET'])
+def notesList(request):
+    notes = Note.objects.all()
+    serializer = NoteSerializer(notes, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def noteDetail(request, pk):
+    note = Note.objects.get(id=pk)
+    serializer = NoteSerializer(note, many=False)
+    return Response(serializer.data)
